b5043ffbd7d15807a25ae720a155fd8879c2513b
Oscar Roos <oscarrroos123@gmail.com>
2025-11-30 16:38:39 +0100

    Added SQL scripts; forced white background for all tabs

---
+++ SQL/insert_user.sql
@@ -0,0 +1,34 @@
+INSERT INTO users (
+        id,
+        phone,
+        name,
+        email,
+        email_verified,
+        address_street,
+        address_postal_code,
+        address_region,
+        company,
+        accepted_terms,
+        accepted_terms_at,
+        accepted_terms_version,
+        created_at,
+        updated_at,
+        role
+    )
+VALUES (
+        'cmic0pmax0000ume0vhr3dwol',
+        '+4745938863',
+        'John Doe',
+        NULL,
+        NULL,
+        'Norway Street 1',
+        '1234',
+        'Oslo',
+        'BCG',
+        TRUE,
+        NULL,
+        NULL,
+        NOW(),
+        NOW(),
+        'super_admin'
+    );
